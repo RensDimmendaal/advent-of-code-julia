b34a48ff7f90dfb0750be39032256d9b24dee5a3
rensdimmendaal <rens@dimmendaal.com>
2020-12-01 08:09:30 +0100

    pluto template

---
+++ template.jl
@@ -0,0 +1,38 @@
+### A Pluto.jl notebook ###
+# v0.12.15
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 2bf8e4c2-339b-11eb-1d49-7f1b210a650a
+using DelimitedFiles
+
+# ╔═╡ 3e0c149a-3350-11eb-0b17-7bea5d1a28d4
+begin
+	DAY = 1
+	md"# 2020 Day $DAY"
+end
+
+# ╔═╡ 18080fe2-339b-11eb-34f0-f393638506fc
+md"## Part 1"
+
+# ╔═╡ 4e084a4e-339b-11eb-3ffc-41118215f226
+md"*<Insert Description>*"
+
+# ╔═╡ 24e1613c-339b-11eb-2976-59884053d1ee
+input = readdlm("../data/day_$DAY.txt", ' ', Int, '\n');
+
+# ╔═╡ 5f797e22-339d-11eb-1ff3-dda90f946838
+md"## Part 2"
+
+# ╔═╡ e3177eae-339e-11eb-3f9f-1f7230976832
+md"*<Insert Description>*"
+
+# ╔═╡ Cell order:
+# ╠═3e0c149a-3350-11eb-0b17-7bea5d1a28d4
+# ╠═2bf8e4c2-339b-11eb-1d49-7f1b210a650a
+# ╟─18080fe2-339b-11eb-34f0-f393638506fc
+# ╠═4e084a4e-339b-11eb-3ffc-41118215f226
+# ╠═24e1613c-339b-11eb-2976-59884053d1ee
+# ╟─5f797e22-339d-11eb-1ff3-dda90f946838
+# ╟─e3177eae-339e-11eb-3f9f-1f7230976832
